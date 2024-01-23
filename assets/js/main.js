8e51a8cc970a45ed7c9dd5289eeb6b65fc55e4b2
Mosys2 <mohammadsaadati117@gmail.com>
2024-01-23 21:10:22 +0330

    index page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,24 @@
+$('.slider-blog').slick({
+    //dots:true,
+    infinite: true,
+    rtl: true,dot:true,
 
-
-//categ
-$(".accordion-item .accordion-header .accordion-button").att
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 4000,
+    easing: 'linear',
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                centerPadding: '0px',
+                centerMode: true,
+                slidesToShow: 1,
+                infinite: true,
+                variableWidth: true
+            }
+        }
+    ]
+});
